6de6aed0e2750947820c43c3aecd9d2fa77ea543
Aashish Panthee <aashishpanthee060@gmail.com>
2026-01-02 22:46:57 +0545

    fix: display backend validation errors in Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,50 +1,53 @@
+import { FormInput, SubmitBtn } from '@/components';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { axiosInstance } from '@/config';
+import { toast } from '@/hooks/use-toast';
+import { useAppDispatch } from '@/redux-store/hooks';
+import { type ReduxStore } from '@/redux-store/store';
+import { loginUser } from '@/redux-store/user/userSlice';
+import { AxiosError, AxiosResponse } from 'axios';
 import {
   Form,
   Link,
   redirect,
-  type ActionFunction,
+  useActionData,
   useNavigate,
+  type ActionFunction,
 } from 'react-router-dom';
-import { AxiosError, AxiosResponse } from 'axios';
-import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import { SubmitBtn, FormInput } from '@/components';
-import { toast } from '@/hooks/use-toast';
-import { axiosInstance } from '@/config';
-import { type ReduxStore } from '@/redux-store/store';
-import { loginUser } from '@/redux-store/user/userSlice';
-import { useAppDispatch } from '@/redux-store/hooks';
 
 export const action =
   (store: ReduxStore): ActionFunction =>
-  async ({ request }): Promise<Response | null> => {
-    const formData = await request.formData();
-    const data = Object.fromEntries(formData);
-    try {
-      const response: AxiosResponse = await axiosInstance.post(
-        '/auth/local',
-        data
-      );
-      const username = response.data.user.username;
-      const jwt = response.data.jwt;
-      store.dispatch(loginUser({ username, jwt }));
-      return redirect('/');
-    } catch (error) {
-      console.log(error);
-      const errorMesg =
-        error instanceof AxiosError
-          ? error.response?.data.error.message
-          : 'Login failed';
-      toast({
-        description: errorMesg,
-      });
-      return null;
-    }
-  };
+    async ({ request }): Promise<Response | { error: string } | null> => {
+      const formData = await request.formData();
+      const data = Object.fromEntries(formData);
+      try {
+        const response: AxiosResponse = await axiosInstance.post(
+          '/auth/local',
+          data
+        );
+        const username = response.data.user.username;
+        const jwt = response.data.jwt;
+        store.dispatch(loginUser({ username, jwt }));
+        return redirect('/');
+      } catch (error) {
+        console.log(error);
+        const errorMesg =
+          error instanceof AxiosError
+            ? error.response?.data.error.message
+            : 'Login failed';
+        toast({
+          description: errorMesg,
+          variant: 'destructive',
+        });
+        return { error: errorMesg }
+      }
+    };
 
 function Login() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const actionData = useActionData() as { error?: string } | undefined;
 
   const loginAsGuestUser = async (): Promise<void> => {
     try {
@@ -71,7 +74,11 @@ function Login() {
           <Form method='POST'>
             <FormInput type='email' label='email' name='identifier' />
             <FormInput type='password' name='password' />
-
+            {actionData?.error && (
+              <div className='text-sm text-red-500 '>
+                {actionData.error}
+              </div>
+            )}
             <SubmitBtn text='Login' className='w-full mt-4' />
             <Button
               type='button'
