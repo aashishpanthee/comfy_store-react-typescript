f52f7579c2294cad7fa20fd9db32355251e51102
Aashish Panthee <aashishpanthee060@gmail.com>
2026-01-02 23:58:19 +0545

    fix: remove irrelevant paragraph & add new content

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,16 +4,16 @@ import HeroCarousel from './HeroCarousel';
 
 function Hero() {
   return (
-    <section className=' grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+    <section className='grid items-center grid-cols-1 gap-24  lg:grid-cols-2'>
       <div>
         <h1 className='max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl '>
           We’re changing the way people shop.
         </h1>
-        <p className='mt-8 max-w-xl text-lg leading-8'>
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-          qui lorem cupidatat commodo.
+        <p className='max-w-xl mt-8 text-lg leading-8'>
+          Discover thoughtfully crafted furniture, cozy textiles, and modern
+          decor all in one place. From everyday essentials to statement pieces,
+          we curate quality items that make your home feel welcoming, stylish,
+          and uniquely yours.
         </p>
         <Button asChild size='lg' className='mt-10'>
           <Link to='/products'>Our Products</Link>
