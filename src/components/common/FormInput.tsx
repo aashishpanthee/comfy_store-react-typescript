0c1f8dd05b7707033a706d2f7f5bd6efeaa8fbc2
Aashish Panthee <aashishpanthee060@gmail.com>
2026-01-02 21:22:15 +0545

    fix: add spread operator for rest of the props in the Input

--- src/components/common/FormInput.tsx
+++ src/components/common/FormInput.tsx
@@ -1,20 +1,21 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { InputHTMLAttributes } from 'react';
 
-type FormInputProps = {
+type FormInputProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   type: string;
   label?: string;
   defaultValue?: string;
 };
 
-function FormInput({ label, name, type, defaultValue }: FormInputProps) {
+function FormInput({ label, name, type, defaultValue, ...rest }: FormInputProps) {
   return (
     <div className='mb-2'>
       <Label htmlFor={name} className='capitalize'>
         {label || name}
       </Label>
-      <Input id={name} name={name} type={type} defaultValue={defaultValue} />
+      <Input id={name} name={name} type={type} defaultValue={defaultValue} {...rest} />
     </div>
   );
 }
