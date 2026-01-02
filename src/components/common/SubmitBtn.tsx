216c69bbaf5c7e40452fbaa529207a060a0b6a5e
Aashish Panthee <aashishpanthee060@gmail.com>
2026-01-02 21:21:14 +0545

    fix: add disable attribute in the input

--- src/components/common/SubmitBtn.tsx
+++ src/components/common/SubmitBtn.tsx
@@ -1,18 +1,20 @@
-import { useNavigation } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { ReloadIcon } from '@radix-ui/react-icons';
+import { useNavigation } from 'react-router-dom';
 
 const SubmitBtn = ({
   text,
   className,
+  disabled,
 }: {
   text: string;
   className?: string;
+  disabled?: boolean;
 }) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   return (
-    <Button type='submit' className={className} disabled={isSubmitting}>
+    <Button type='submit' className={className} disabled={isSubmitting || disabled}>
       {isSubmitting ? (
         <span className='flex '>
           <ReloadIcon className='w-4 h-4 mr-2 animate-spin' />
