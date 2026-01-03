9cf3c3864e9975c26779d5e7e33df8d5fd2c2980
Aashish Panthee <aashishpanthee060@gmail.com>
2026-01-03 15:32:22 +0545

    fix: remove ShimmerCard

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,17 +1,14 @@
 import { Header, Navbar } from '@/components';
-import Loading from '@/components/common/Loading';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 function HomeLayout() {
-  const navigation = useNavigation();
-  const isPageLoading = navigation.state === 'loading';
 
   return (
     <>
       <Header />
       <Navbar />
-      <div className='align-element py-20'>
-        {isPageLoading ? <Loading /> : <Outlet />}
+      <div className='py-20 align-element'>
+        <Outlet />
       </div>
     </>
   );
