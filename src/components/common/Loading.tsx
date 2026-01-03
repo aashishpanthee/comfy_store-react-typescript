bc072db498a6b193128d30d8fbcd0c4ad944d2fd
Aashish Panthee <aashishpanthee060@gmail.com>
2026-01-03 13:27:08 +0545

    fix: update Loading Component with more skeleton

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from '../ui/skeleton';
 
 function Loading() {
   return (
-    <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 '>
-      {Array.from({ length: 3 }).map((_, index) => {
+    <div className='grid gap-8 pt-12 md:grid-cols-2 lg:grid-cols-3 '>
+      {Array.from({ length: 9 }).map((_, index) => {
         return (
-          <div key={index} className='flex flex-col space-y-3'>
+          <div key={index} className='flex flex-col space-y-4'>
             <Skeleton className='h-[125px] w-full rounded-xl' />
             <div className='space-y-2'>
               <Skeleton className='h-4 mx-auto w-[250px]' />
